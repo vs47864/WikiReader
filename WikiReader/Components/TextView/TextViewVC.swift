cff1e30751396d8606c0271584a15e6a196ecc76
Višeslav Šako <Viseslav.Sako@asseco-see.hr>
2018-08-28 01:14:57 +0200

    Done UI.

---
+++ WikiReader/Components/TextView/TextViewVC.swift
@@ -0,0 +1,35 @@
+//
+//  TextViewVC.swift
+//  WikiReader
+//
+//  Created by Višeslav Šako on 28/08/2018.
+//  Copyright © 2018 viseslav. All rights reserved.
+//
+
+import UIKit
+
+class TextViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
